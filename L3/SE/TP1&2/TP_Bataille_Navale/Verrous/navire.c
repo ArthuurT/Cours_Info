d4dd7acf8a8269b39e20280ad96df2a2376eb409
WarNN <muctutur72@hotmail.fr>
2017-10-07 15:47:39 +0200

    Suivi SE

--- L3/SE/TP1&2/TP_Bataille_Navale/Verrous/navire.c
+++ L3/SE/TP1&2/TP_Bataille_Navale/Verrous/navire.c
@@ -20,6 +20,13 @@ main( int nb_arg , char * tab_arg[] )
   case_t marque = MER_CASE_LIBRE ;
   char nomprog[128] ;
   float energie = 0.0 ;
+  int rowmer;
+  int colmer;
+  off_t taillemer;
+  bateau_t * bateau;
+  off_t offset;
+  int taille_bateau;
+
 
   /* variables utilisation verrou */
 
@@ -75,10 +82,14 @@ main( int nb_arg , char * tab_arg[] )
 
   sprintf(VerrouPosition,"VerrouPosition_%s",fich_mer);
 
+  mer_dim_lire(fd1,&rowmer,&colmer);
+
+  mer_lc2pos(fd1,rowmer,colmer,&taillemer);
+
   verrouPosition.l_type = F_WRLCK;
   verrouPosition.l_whence = 0;
   verrouPosition.l_start = 0;
-  verrouPosition.l_len = 10;
+  verrouPosition.l_len = taillemer;
 
   fcntl(fd1,F_SETLKW,verrouPosition);
 
@@ -86,12 +97,16 @@ main( int nb_arg , char * tab_arg[] )
 
   /* Pose du bateau sur la mer */
 
-  if(mer_bateau_initialiser(fd1,bateau_new(coords_new(), marque, getpid())) == 0){
+  bateau = bateau_new(coords_new(), marque, getpid());
+
+  if(mer_bateau_initialiser(fd1,bateau) == 0){
     printf("Pose du bateau sur la mer --> SUCCESS\n");
   }else{
     printf("Pose du bateau sur la mer --> FAILURE\n");
     exit(1);
   }
+
+  coords_printf(bateau->corps);
 
   /* Destruction du verrou après que le bateau soit posé */
 
@@ -116,10 +131,15 @@ main( int nb_arg , char * tab_arg[] )
 
     fd1 = open(fich_mer, O_RDONLY);
 
+    coord_position_get(coords_coord_get(bateau->corps,1));
+    offset = coord_position_get(coords_coord_get(bateau->corps,1));
+    taille_bateau = coords_nb_get(bateau->corps); /* A MODIFIER SI BATEAU EN COLONNE */
+    lseek(fd1,offset,SEEK_SET);
+
     verrouBouclier.l_type = F_WRLCK;
-    verrouBouclier.l_whence = 0;
+    verrouBouclier.l_whence = 1;
     verrouBouclier.l_start = 0;
-    verrouBouclier.l_len = 10;
+    verrouBouclier.l_len = taille_bateau;
 
     fcntl(fd1,F_SETLKW,verrouBouclier);
     close(fd1);
