83027a70cae4329c7b9fd6d5017807afcb139cd1
WarNN <muctutur72@hotmail.fr>
2017-11-14 12:11:17 +0100

    dshdb

--- L3/SE/TP3/tempsmoy.c
+++ L3/SE/TP3/tempsmoy.c
@@ -3,24 +3,24 @@
 #include <sys/time.h>
 #include <string.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
 
 #define TAILLE_COMMANDE 20
 
-int compare (void const *a, void const *b)
+int floatcomp(const void* elem1, const void* elem2)
 {
-   int const *pa = a;
-   int const *pb = b;
-
-   return *pa - *pb;
+    if(*(const float*)elem1 < *(const float*)elem2)
+        return -1;
+    return *(const float*)elem1 > *(const float*)elem2;
 }
 
-int tempsMoyen(float * tab, int n){
+float tempsMoyen(float * tab, int n){
 
-	if(n % 2 == 1) return tab[(n / 2) + 1];
-	else return ((tab[n / 2] + tab[(n / 2)+ 1]) / 2);
+	if(n % 2 == 1) return tab[n / 2];
+	else return ((tab[n / 2] + tab[(n / 2)- 1]) / 2);
 }
 
 
@@ -34,12 +34,11 @@ int main(int argc, char * argv[]){
 	float moyenne;
 	int tube[2];
 	float tempsPs;
-	int pid_fils;
 	int cr, cr1;
 
 	char commande[TAILLE_COMMANDE];
 
-	struct timeval t1, t2;
+	struct timeval t1, t2,res;
 
 
 	if(argc != 4){
@@ -51,75 +50,110 @@ int main(int argc, char * argv[]){
 	nb_processus = atoi(argv[3]);
 	strcpy(commande,argv[2]);
 
-
-	printf("nb_repet = %i\n",nb_repet);
-	printf("nb_processus = %i\n",nb_processus);
-
 	float M[nb_processus];					/*  Contient les les temps moyens d'executions de chaque processus Mi = Ti / K */
-	int tab_pid[nb_processus];				/*  Contient les pid de chaque processus fils */
 	int fd_devnull = open("/dev/null", O_RDWR);
 
-	pipe(tube);
+  pipe(tube);
 
 	/* Création de nb_processus fils */
 
 	for(i = 0; i < nb_processus; i++){
-		switch(tab_pid[i] = fork()){
+		switch(fork()){
 
 			case -1:		perror("Erreur fork n°1");
-							exit(1);
+							    exit(1);
 
-			case 0:			close(tube[1]);
-							close(0);
-							dup(tube[0]);
-							close(tube[0]);
+			case 0:			close(tube[0]);
 
-							gettimeofday(&t1,NULL);
+							    gettimeofday(&t1,NULL);
 
-							/* Creation de nb_repet processus sous-fils */
+    							/* Creation de nb_repet processus sous-fils */
 
-							for(j = 0; j < nb_repet; j++){
-								switch(fork()){
+    							for(j = 0; j < nb_repet; j++){
+    								switch(fork()){
 
-									case -1: 		
-													perror("Erreur fork n°2");
-													exit(1);
+    								  case -1: perror("Erreur fork n°2");
+    													 exit(1);
 
-									case 0:			dup2(fd_devnull,1);
-													close(fd_devnull);
-													execlp(commande,commande,NULL);
+    									case 0:	dup2(fd_devnull,1);
+    													close(fd_devnull);
+    													execlp(commande,commande,NULL);
+                              exit(3);
 
-								}
-							}
+								   }
+							    }
 
-							while(wait(&cr1) != -1);
+							    while(wait(&cr1) != -1){
+                    if(cr1 > 768){
+                      exit(2);
+                    }
+                    else if(cr1 != 0){
+                      exit(cr1);
+                    }
+                  }
 
-							gettimeofday(&t2,NULL);
+							    gettimeofday(&t2,NULL);
 
-							/*	
-							 *	On soustrait t2 le temps final à t1 le temps initial -> on obtient T le temps d'execution.
-							 *	Il suffit de divisé le tous par le nombre de repetition pour obtenir le temps moyen.
-							 */
+    							/*
+    							 *	On soustrait t2 le temps final à t1 le temps initial -> on obtient T le temps d'execution.
+    							 *	Il suffit de divisé le tous par le nombre de repetition pour obtenir le temps moyen.
+    							 */
 
-							tempsPs = ((t2.tv_sec + t2.tv_usec) - (t1.tv_sec + t1.tv_usec)) / nb_repet;
-							exit(tempsPs);
 
-			default:	close(tube[0]);
-						printf("Programme père\n");	
-		}
-	}
+                  if (t2.tv_sec > t1.tv_sec)
+                      timersub(&t2,&t1,&res);
+                  else if (t2.tv_sec < t1.tv_sec)
+                       timersub(&t1, &t2, &res);
+                   else  // t2.tv_sec == t1.tv_sec
+                   {
+                       if (t2.tv_usec >= t1.tv_usec)
+                           timersub(&t2, &t1, &res);
+                       else
+                           timersub(&t1, &t2, &res);
+                   }
 
-	/* On récupère le temps moyen d'execution de chaque processus */
+    							tempsPs = res.tv_sec + res.tv_usec / nb_repet;
+                  tempsPs = tempsPs / 1000;
+    							write(tube[1],&tempsPs,sizeof(float));
+                  close(tube[1]);
+                  exit(0);
+	 }
+ }
 
-	while((pid_fils = wait(&cr)) != -1){
-		for(k = 0; tab_pid[i] != pid_fils; k++);
-		M[k] = tempsPs;
-	}
+  for(i = 0; (i < nb_processus) && (wait(&cr) != -1); i++){
+    printf("cr = %i\n",cr);
+    if(cr != 0){
+      printf("cr = %i\n",cr);
+      if(cr == 512){
+          printf("Erreur commande inexistante\n");
+      }else if(cr > 128){
+          printf("Erreur cr = %i\n", cr);
+      }else{
+          printf("Interrompu par le signal n° %i",cr);
+      }
+      exit(-1);
+    }else{
+      read(tube[0],&M[i],sizeof(float));
+    }
+  }
 
-	qsort(M,nb_processus,sizeof(float),compare);
+  printf("Avant le tri \n");
+  for(i = 0; i < nb_processus; i++){
+    printf("M[%i] = %f\n",i,M[i]);
+  }
+
+	qsort(M,nb_processus,sizeof(float),floatcomp);
+
+  printf("\n");
+
+  printf("Apres le tri \n");
+  for(i = 0; i < nb_processus; i++){
+    printf("M[%i] = %f\n",i,M[i]);
+  }
+
 	moyenne = tempsMoyen(M,nb_processus);
 
-	printf("\n\n En moyenne la commande '%s' met %f secondes à s'executer\n\n",commande,moyenne);
+	printf("\n\nEn moyenne la commande '%s' met %f secondes à s'executer\n\n",commande,moyenne);
 
 
 }
